066a7511b5a7ba80b36e3b6b7d5a735631d78d28
Yasmine C <yasmine.chaiboubi@le-campus-numerique.fr>
2025-01-06 15:29:32 +0100

    Exo 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,13 @@
 // you can write js here
-console.log('hello from file');
+let kelvins = 294;
+// Conversion en Celsius
+let celsius = kelvins - 273;
+// La température en Fahrenheit se calcule avec la formule : Fahrenheits = Celsius * (9/5) + 32.
+let fahrenheits = celsius * (9/5) + 32;
+// La méthode .floor() arrondit le nombre de Fahrenheit à l'entier inférieur.
+fahrenheits = Math.floor(fahrenheits);
+kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+// Afficher les résultats dans la console du navigateur
+console.log("Température en Celsius : " + celsius);
+console.log("Température en Fahrenheit : " + fahrenheits);
